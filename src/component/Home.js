362513e3e412caf77e31885b299ea68d1fd998e8
getoffersback <akkashgupta2@gmail.com>
2024-10-29 23:20:30 +0530

    Update Home.js

--- src/component/Home.js
+++ src/component/Home.js
@@ -73,6 +73,34 @@ const Home = () => {
               className="bg-white mt-1 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
             {error && <small className="text-red-500 my-1">Please enter valid mobile number!!</small>}
           </div>
+                <div
+    style={{
+        backgroundColor: 'rgb(241, 245, 249)',
+        opacity: 1,
+        display: 'flex',
+        alignItems: 'center',
+        padding: '0.5rem',
+        borderRadius: '0.75rem',
+        marginTop: '1.25rem',
+    }}
+>
+    <svg
+        stroke="currentColor"
+        fill="currentColor"
+        strokeWidth="0"
+        viewBox="0 0 24 24"
+        style={{ color: 'rgb(234, 179, 8)', marginRight: '0.5rem' }}
+        height="40"
+        width="40"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path d="M20.749 12l1.104-1.908a1 1 0 0 0-.365-1.366l-1.91-1.104v-2.2a1 1 0 0 0-1-1h-2.199l-1.103-1.909a1.008 1.008 0 0 0-.607-.466.993.993 0 0 0-.759.1L12 3.251l-1.91-1.105a1 1 0 0 0-1.366.366L7.62 4.422H5.421a1 1 0 0 0-1 1v2.199l-1.91 1.104a.998.998 0 0 0-.365 1.367L3.25 12l-1.104 1.908a1.004 1.004 0 0 0 .364 1.367l1.91 1.104v2.199a1 1 0 0 0 1 1h2.2l1.104 1.91a1.01 1.01 0 0 0 .866.5c.174 0 .347-.046.501-.135l1.908-1.104 1.91 1.104a1.001 1.001 0 0 0 1.366-.365l1.103-1.91h2.199a1 1 0 0 0 1-1v-2.199l1.91-1.104a1 1 0 0 0 .365-1.367L20.749 12zM9.499 6.99a1.5 1.5 0 1 1-.001 3.001 1.5 1.5 0 0 1 .001-3.001zm.3 9.6-1.6-1.199 6-8 1.6 1.199-6 8zm4.7.4a1.5 1.5 0 1 1 .001-3.001 1.5 1.5 0 0 1-.001 3.001z" />
+    </svg>
+    <p style={{ fontSize: '12px', fontWeight: '600' }}>
+        Get Free OTTs on Recharge with a ₹149 plan
+    </p>
+</div>
+
           <div className="mt-5">
             <button onClick={openOffer} className="bg-blue-500 py-3 w-full text-[15px] rounded-xl font-bold text-white">Recharge</button>
           </div>
